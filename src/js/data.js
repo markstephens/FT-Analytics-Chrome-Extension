c96e04fe63ef1344440ae6b33c7552348b521140
Mark Stephens <Mark.Stephens@ft.com>
2012-11-13 17:28:27 +0000

    First stab at data

--- src/js/data.js
+++ src/js/data.js
@@ -1,22 +1,43 @@
 if(typeof(ftaceData)==="undefined"){
     var ftaceData = (function(){
-        var userTypes;
+        var links = {}, userTypes;
 
         return {
             init : function() {
                 $(document).ready(function(){
                     chrome.extension.sendMessage({}, function(ftace){
 
-                        $.ajax({
-                            url : 'https://realtime.ijento.com/financialtimes/ft-subs-articles.txt',
-                            dataType: "text",
-                            success : function(data, textStatus, jqXHR) {
-                                userTypes = $.parseJSON(data);
+                        $('a[href^="http://www.ft.com/cms"]').each(function() {
+                            var link = $(this),
+                                uuid = link.attr('href').match(/http:\/\/www.ft.com\/cms(\/s)?\/\d\/([\w\-]+).html/);
+
+                            if(uuid) {
+                                links[uuid[2]] = link;
                             }
                         });
 
-                        $('#page-container').wrap('<div id="'+ftace.prefix+'DataWrapper" style="width:'+$('#page-container').width()+'px; margin: 0 auto; z-index:0"></div>');
-                        $('#page-container').addClass(ftace.prefix+'Data');
+                        console.log(links);
+
+                        $.ajax({
+                            url : 'https://realtime.ijento.com/financialtimes/ft-subs-articles.txt',
+                            dataType: "json",
+                            success : function(data, textStatus, jqXHR) {
+                                userTypes = data.articles;
+
+                                for(var i=0;i<userTypes.length;i++) {
+                                    if(links.hasOwnProperty(userTypes[i].uuid)) {
+                                        links[userTypes[i].uuid].after([
+                                            '<span class="',ftace.prefix,'data">',
+                                            'Impressions: ', userTypes[i].impressions,' ',
+                                            'Anonymous: ', userTypes[i].anon,' ',
+                                            'Registered: ', userTypes[i].reg,' ',
+                                            'Subscriber: ', userTypes[i].subs,' ',
+                                            '</span>'
+                                        ].join(''))
+                                    }
+                                }
+                            }
+                        });
                     });
                 });
             },
@@ -27,8 +48,7 @@ if(typeof(ftaceData)==="undefined"){
 
             stop : function(){
                 chrome.extension.sendMessage({}, function(ftace){
-                    $('#'+ftace.prefix+'DataWrapper').attr('id','');
-                    $('#page-container, [data-pos] a').removeClass(ftace.prefix+'Data');
+
                 });
             }
         }
